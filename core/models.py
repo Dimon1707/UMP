852fbd2888d46e17e7cd78ad4fb204f1ffb48c5d
Семён <Semen8691>
2022-05-20 01:20:40 +1000

    register,profile

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Registration
+
+class Student(User):
+    age = models.IntegerField(null=False)
+    specialization = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return  self.last_name
+
